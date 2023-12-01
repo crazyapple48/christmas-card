c130bc7b809da05c709a561f8889ba64e0030085
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2023-11-30 21:53:35 -0700

    add starting logic for poem iteration

--- src/components/PoetryCard.js
+++ src/components/PoetryCard.js
@@ -1,6 +1,30 @@
 import Poetry from "./Poetry";
 import Picture from "./Picture";
 
+// const poetryArray = [
+//   "Roses are Red",
+//   "Violets are Blue",
+//   "Kayla smells like poo",
+//   "Huddy does too",
+// ];
+
+// const imgArray = [
+//   {
+//     src: "../images/red-rose.jpg",
+//     alt: "Red Rose",
+//   },
+//   {
+//     src: "../images/blue-violet.jpg",
+//     alt: "Blue Violet",
+//   },
+// ];
+
+// let poetryLine = "";
+
+// let currentImg = {};
+
+// const interval = setInterval((poetryArray, imgObject) => {}, interval);
+
 export default function PoetryCard() {
   return (
     <>
