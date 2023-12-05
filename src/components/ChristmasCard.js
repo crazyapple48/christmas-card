7c48f3c5f00fd1ba7543d322998e33020840dc88
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2023-12-04 17:57:01 -0700

    Add image and text to christmas card/ add background image

--- src/components/ChristmasCard.js
+++ src/components/ChristmasCard.js
@@ -1,7 +1,15 @@
 import React from "react";
+import christmasCard from "../images/ChristmasCard.jpg";
 
 const ChristmasCard = () => {
-  return <div>ChristmasCard</div>;
+  return (
+    <>
+      <div className="christmas-container">
+        '<h1 className="christmas-declaration">Merry Chrismas!!!</h1>
+        <img src={christmasCard} alt="Us at Enchant" id="christmas-card" />
+      </div>
+    </>
+  );
 };
 
 export default ChristmasCard;
