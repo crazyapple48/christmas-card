837c55865b59012ab2e3b18d90376824a5d1d8c1
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2023-12-19 17:36:27 -0700

    Moved poetry styles to Poetry.module.css

--- src/components/Poetry/Poetry.js
+++ src/components/Poetry/Poetry.js
@@ -1,5 +1,5 @@
 import styles from "./Poetry.module.css";
 
 export default function Poetry({ poetryLine }) {
-  return <p className="poetry">{poetryLine}</p>;
+  return <p className={styles.poetry}>{poetryLine}</p>;
 }
